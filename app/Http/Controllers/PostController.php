3b3f909f37b490374ace33590b9aeb250eae1c2b
cfuentealba <cfuentealba@netred.cl>
2023-05-03 19:23:10 -0400

    Vista

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    public function mostrarPosts($userId)
+    public function mostrarPosts()
     {
+        $userId = Auth::user()->id;
         $posts = Post::with('comentarios')->where('user_id', $userId)->get();
 
         return view('posts', ["posts" => $posts]);
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+//Route::get('/', function () {
+//    return view('posts');
+//});
 
 Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/posts/{userId}', 'App\Http\Controllers\PostController@mostrarPosts')->name('posts');
+Route::get('/', 'App\Http\Controllers\PostController@mostrarPosts')->name('posts');
